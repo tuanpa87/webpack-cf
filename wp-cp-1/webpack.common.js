97f20eff2b0dcfdd614e714d4f7bd62de36d9be5
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-07-14 16:14:44 +0700

    Splitting Dev & Production

--- wp-cp-1/webpack.common.js
+++ wp-cp-1/webpack.common.js
@@ -1,13 +1,8 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
