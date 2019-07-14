ce84cf5c011df84a6c3d46168ce9790abd87cdc5
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-07-14 22:55:58 +0700

    mutil hmtl

--- wp-cp-2/webpack.config.js
+++ wp-cp-2/webpack.config.js
@@ -41,6 +41,18 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+            }
+          }
+        ],
+        exclude: path.resolve(__dirname, "src/index.html"),
       }
     ]
   },
