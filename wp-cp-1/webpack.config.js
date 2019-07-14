8985b0d18a197e19df56093ae10a4f6b1520ab37
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-07-14 15:33:41 +0700

    sass loader

--- wp-cp-1/webpack.config.js
+++ wp-cp-1/webpack.config.js
@@ -10,9 +10,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-}
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  }
+};
