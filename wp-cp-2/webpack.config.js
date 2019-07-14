a51a2c865e46a6e3bdd29dd3d060b99fb5e7f5aa
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-07-14 22:15:32 +0700

    HTML + IMAGE LOADERS

--- wp-cp-2/webpack.config.js
+++ wp-cp-2/webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/js/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist"
+    //publicPath: "/dist"
   },
   module: {
     rules: [
@@ -22,8 +24,31 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/"
+            }
+          }
+        ]
       }
     ]
   },
-  plugins: [new MiniCssExtractPlugin({ filename: "main.css" })]
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "main.css" }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new CleanWebpackPlugin()
+  ]
 };
