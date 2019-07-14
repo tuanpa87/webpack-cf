f18a45bad2e983b77db4b80f2f8a3cdc56746603
tuanpa87 <tuanpa87@users.noreply.github.com>
2019-07-14 20:05:55 +0700

    Extract CSS & Minify HTML/CSS/JS

--- wp-cp-1/webpack.common.js
+++ wp-cp-1/webpack.common.js
@@ -1,22 +1,12 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into CommonJS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
